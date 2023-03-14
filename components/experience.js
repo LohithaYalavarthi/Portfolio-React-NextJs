0c08a8fd35d4ccf03d92923981ec0939be66b621
Lohitha <lohithayalavarthi95@gmail.com>
2023-03-14 11:54:39 -0400

    space changes

--- components/experience.js
+++ components/experience.js
@@ -46,10 +46,10 @@ function Experience() {
             subtext1="Apr. 2019 to Jan. 2021 - India"
             points={[
               "Implemented responsive front-end UI/UX, back-end REST APIs, and Mobile applications using ReactJS,NodeJS, NextJS, Contentful",
-              "Discussed business requirements with clients, created stories and collaborated with designers to build UImockups and deliver quality-driven applications.",
+              "Discussed business requirements with clients, created stories and collaborated with designers to build UI mockups and deliver quality-driven applications.",
               "Handled different projects, worked independently and in a team, and mentored other developers",
-              "Experienced working with AWS SAM framework for building serverless applications and created Lambdafunctions, API Gateways, Databases",
-              "Experienced working with version control GIT, GitHub, NPM, yarn, debugging, and familiar withmicroservices, (CI/CD) pipelines i.e. Jenkins and cloud technologies like AWS, Docker, Kubernetes.",
+              "Experienced working with AWS SAM framework for building serverless applications and created Lambda functions, API Gateways, Databases",
+              "Experienced working with version control GIT, GitHub, NPM, yarn, debugging, and familiar with microservices, (CI/CD) pipelines i.e. Jenkins and cloud technologies like AWS, Docker, Kubernetes.",
             ]}
           />
           <BoxView
@@ -61,9 +61,9 @@ function Experience() {
               "Built ReactJS/React Native app and web apps for banking clients (e.g., Equitas, India Post) to optimizein-branch experiences, enable efficient service, and enhance customer accessibility for on-the-go banking",
               "Responsible for creating and managing content using contentful and used set of JSON APIs to query thisdata in our React application.",
               "Experience working with relational databases ( PostgreSQL, MySQL , etc), NoSQL databases ( MongoDB,DynamoDB, etc), and object stores (S3, etc).",
-              "Developed applications with SEO principles, the best software practices, and security measures, UXdesign, and provided coding reviews.",
-              "Developed the application with zero issues and improved the app quality, which increased the number ofusers using the app from 30% to 90%.",
-              "Involved in various phases of Software Development life cycle (SDLC) i.e. waterfall, agile softwaredevelopment practices such as SCRUM, KANBAN.",
+              "Developed applications with SEO principles, the best software practices, and security measures, UX design, and provided coding reviews.",
+              "Developed the application with zero issues and improved the app quality, which increased the number of users using the app from 30% to 90%.",
+              "Involved in various phases of Software Development life cycle (SDLC) i.e. waterfall, agile software development practices such as SCRUM, KANBAN.",
             ]}
           />
         </Paper>
